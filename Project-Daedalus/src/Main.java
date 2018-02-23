2173012138d717350bc6fdc493763418d44567d4
Chris Nguyen <chrisnguyen2000@hotmail.com>
2018-02-23 14:00:09 -0600

    Updated header of the main class

--- Project-Daedalus/src/Main.java
+++ Project-Daedalus/src/Main.java
@@ -1,12 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Project-Daedalus
+ *
+ * Digital Tabletop map utility with token integration
  */
 
 /**
- *
- * @author Chris
+ * @author Christopher Nguyen
+ *          Eric Busch
+ *          Tyler Eley
+ *          William H
  */
 public class Main {
 
