86ce4856c278304d413491a065fe8b8941b5f2a9
Chris Nguyen <chrisnguyen2000@hotmail.com>
2018-02-24 14:11:55 -0600

    Implemented the image object

--- Project-Daedalus/src/Token.java
+++ Project-Daedalus/src/Token.java
@@ -1,14 +1,36 @@
-/*
+/* Project-Daedalus
     This is the token interface for the player tokens.
     It is a generalized player token so that we can
     implement player tokens from different tabletop games
 */
 
-import javax.swing.*;
+/**
+ * @author Christopher Nguyen
+ *          Eric Busch
+ *          Tyler Eley
+ *          William Hopkins
+ */
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.*;
 
-public interface Token
+public abstract class Token
 {
-    //implement image variable
+    // PARAMETERS
+    Image token;
     
-    public String DisplayStats();
+    // ACCESSORS
+    public Image getMap()
+    {   return token; }
+    public void setMap( Image t )
+    {   token = t;  }
+    public void setMap( String s )
+    {
+        try {  token = ImageIO.read( new File( s ) ); }
+        catch ( IOException ex ) {   }
+    }
+    
+    // METHODS
+    abstract public String DisplayStats();
 }
