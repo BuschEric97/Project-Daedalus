5660e6994439faef7cc60d54d58c09a0074e77dc
Chris Nguyen <chrisnguyen2000@hotmail.com>
2018-02-24 12:42:40 -0600

    Constructors and Accessors

--- Project-Daedalus/src/Tile.java
+++ Project-Daedalus/src/Tile.java
@@ -1,13 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* Project-Daedalus
+ *
+ * Digital Tabletop map utility with token integration
  */
 
 /**
- *
- * @author Cytus
+ * @author Christopher Nguyen
+ *          Eric Busch
+ *          Tyler Eley
+ *          William Hopkins
  */
-public class Tile {
+
+public class Tile
+{
+    // PARAMETERS
+    // Image map;
+    Token token;
     
+    public Tile()
+    {
+        //map = null;
+        token = null;
+    }
+    
+    // ACCESSORS
+    /* public Image getMap()
+    {   return map; }
+    public void setMap( Image m )
+    {   map = m;    }*/
+    public Token getToken()
+    {   return token;   }
+    public void setToken( Token t )
+    {   token = t;  }
 }
