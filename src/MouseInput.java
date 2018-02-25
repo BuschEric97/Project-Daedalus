1cec4a26c2b189b53b10489bf890c88136c49d49
Eric Busch <buscheric97@gmail.com>
2018-02-25 03:27:59 -0600

    added logic to MouseInput to handle moving tokens

--- src/MouseInput.java
+++ src/MouseInput.java
@@ -29,12 +29,19 @@ public class MouseInput implements MouseListener
     int ppi; // size of ppi
     int screenWidth; // size (in pixels) of screen width
     int screenHeight; // size (in pixels) of screen height
+    static boolean tokenSelected = false; // boolean variable to determine which coordinate variables to record into
+    static int tokenToMoveX; // x coordinate for token to be moved
+    static int tokenToMoveY; // y coordinate for token to be moved
+    static int moveTokenToX; // x coordinate for tile to move token to
+    static int moveTokenToY; // y coordinate for tile to move token to
     
     @Override
     public void mouseClicked(MouseEvent me)
     {
         int mx = me.getX(); // get the X coordinate of the mouse click
         int my = me.getY(); // get the Y coordinate of the mouse click
+        int cellWidth; // variable for x coordinate of the cell if the user clicked on the grid
+        int cellHeight; // variable for y coordinate of the cell if the user clicked on the grid
                 
         if (mx < marginSize && my < marginSize)
             System.exit(0); // quit the program if the user clicks the top left of the screen
@@ -43,9 +50,30 @@ public class MouseInput implements MouseListener
             System.out.println("User clicked at position: (" + mx + ", " + my + ")"); // print coordinates (in pixels) of mouse click to console
         else
         {
-            int cellWidth = mx / ppi; // use integer division to determine the x coordinate (in the array) of the cell clicked
-            int cellHeight = my / ppi; // use integer division to determine the y coordinate (in the array) of the cell clicked
+            cellWidth = mx / ppi; // use integer division to determine the x coordinate (in the array) of the cell clicked
+            cellHeight = my / ppi; // use integer division to determine the y coordinate (in the array) of the cell clicked
             System.out.println("User clicked in cell: (" + cellWidth + ", " + cellHeight + ")"); // print coordinates (in the array) of mouse click to console
+            
+            if (!tokenSelected)
+            {
+                tokenToMoveX = cellWidth; // set the x coordinate of the cell clicked
+                tokenToMoveY = cellHeight; // set the y coordinate of the cell clicked
+                tokenSelected = true; // set tokenSelected to true for next iteration
+            }
+            else
+            {
+                moveTokenToX = cellWidth; // set the x coordinate of the cell
+                moveTokenToY = cellHeight; // set the y coordinate of the cell
+                tokenSelected = false; // set tokenSelected back to false for next iteration
+                
+                if (mapArray[moveTokenToY][moveTokenToX].getToken() == null) // check that target cell is empty
+                {
+                    mapArray[moveTokenToY][moveTokenToX].setToken(mapArray[tokenToMoveY][tokenToMoveX].getToken()); // copy the selected token into its new position
+                    mapArray[tokenToMoveY][tokenToMoveX].setToken(null); // delete the selected token from its old position
+                }
+                else
+                    System.err.println("error: target cell is not empty!"); // if target cell is not empty, print an error message
+            }
         }
     }
 
