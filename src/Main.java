200cd19f1fbcae2ed7a4b93a94f7b941f0ce0d3f
Chris Nguyen <chrisnguyen2000@hotmail.com>
2018-02-24 23:22:35 -0600

    Redone

--- src/Main.java
+++ src/Main.java
@@ -15,57 +15,37 @@ import java.math.*;
 
 public class Main extends JFrame
 {
-    int ppi;
-    int marginSize;
-    Tile[][] mapArray;
-    private DrawCanvas canvas;
-    
-    public Main()
-    {
-        ppi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
-        marginSize = ppi / 4;
-        canvas = new DrawCanvas();
-        canvas.setPreferredSize( java.awt.Toolkit.getDefaultToolkit().getScreenSize() );
-        Container cp = getContentPane();
-        cp.add(canvas);
-        setTitle("Game Board");
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setUndecorated(true); // remove the title bar
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set the JFrame close operation to exit with the X button
-        pack();
-        addMouseListener(new MouseInput(marginSize)); // add mouse listener
-        setVisible(true); // make the board visible
-    }
-    
-    private class DrawCanvas extends JPanel
-    {
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);     // paint parent's background
-            setBackground(Color.BLACK);  // set background color for this JPanel
-
-            g.setColor(Color.RED);
-            g.fillRect(0, 0, marginSize, marginSize);
-        }
-    }
-    
-    public static void main(String[] args) {
-        // Run the GUI codes on the Event-Dispatching thread for thread safety
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-               new Main(); // Let the constructor do the job
-            }
-        });
-    }
     
     public static Tile[][] createArray(int ppi, int screenHeight, int screenWidth, int marginSize)
     {
         int maxTilesWidth = (screenWidth - (marginSize * 2)) / ppi; // maximum amount of tiles the width of the screen can hold with margins
         int maxTilesHeight = (screenHeight - (marginSize * 2)) / ppi; // maximum amount of tiles the height of the screen can hold with margins
         
-        Tile[][] mapArray = new Tile[maxTilesWidth][maxTilesHeight]; // create the 2-dimentional array that holds the info for each tile
+        Tile[][] mapArray = new Tile[maxTilesHeight][maxTilesWidth]; // create the 2-dimentional array that holds the info for each tile
         
         return mapArray;
+    }
+    
+    public static void main(String[] args)
+    {
+        int screenHeight = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        int screenWidth = (int)java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        int ppi = java.awt.Toolkit.getDefaultToolkit().getScreenResolution();
+        int marginSize = ppi / 4;
+        boolean state = false; // False to edit map, true to move tokens
+        Tile[][] mapArray = createArray( ppi, screenHeight, screenWidth, marginSize );
+        JFrame f = new JFrame("Game Board");
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setUndecorated(true); // remove the title bar
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set the JFrame close operation to exit with the X button
+        f.pack();
+        f.addMouseListener(new MouseInput(marginSize)); // add mouse listener
+        f.setVisible(true); // make the board visible
+        f.setBackground( Color.BLACK );
+        while (true)
+        {
+            
+            f.repaint();
+        }
     }
 }
