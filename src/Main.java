caa8e67f415a5feb64aefe9e3ef4dca47abed514
Chris Nguyen <chrisnguyen2000@hotmail.com>
2018-02-25 05:51:18 -0600

    Optimized the mouse listener

--- src/Main.java
+++ src/Main.java
@@ -67,80 +67,64 @@ public class Main extends JFrame
         f.setContentPane( canvas ); // set canvas to the content pane
         
         // Action Listeners
-        f.addMouseListener(new MouseInput(marginSize, mapArray, ppi, screenHeight, screenWidth)); // add mouse listener
-    }
-    
-    // Mouse listener
-    class MouseInput implements MouseListener
-    {
-        int marginSize; // size (in pixels) of the margins around the grid
-        Tile[][] mapArray; // array with the tiles of the grid
-        int ppi; // size of ppi
-        int screenWidth; // size (in pixels) of screen width
-        int screenHeight; // size (in pixels) of screen height
-        boolean tokenSelected = false; // boolean variable to determine which coordinate variables to record into
-        int tokenToMoveX; // x coordinate for token to be moved
-        int tokenToMoveY; // y coordinate for token to be moved
-        int moveTokenToX; // x coordinate for tile to move token to
-        int moveTokenToY; // y coordinate for tile to move token to
-
-        @Override
-        public void mouseClicked(MouseEvent me)
+        f.addMouseListener(new MouseListener()
         {
-            repaint();
-            int mx = me.getX(); // get the X coordinate of the mouse click
-            int my = me.getY(); // get the Y coordinate of the mouse click
-            int cellWidth; // variable for x coordinate of the cell if the user clicked on the grid
-            int cellHeight; // variable for y coordinate of the cell if the user clicked on the grid
-
-            if (mx < marginSize && my < marginSize)
-                System.exit(0); // quit the program if the user clicks the top left of the screen
-
-            if (mx < marginSize || my < marginSize || mx > (screenWidth - marginSize) || my > (screenHeight - marginSize))
-                System.out.println("User clicked at position: (" + mx + ", " + my + ")"); // print coordinates (in pixels) of mouse click to console
-            else
+            boolean tokenSelected = false; // boolean variable to determine which coordinate variables to record into
+            int tokenToMoveX; // x coordinate for token to be moved
+            int tokenToMoveY; // y coordinate for token to be moved
+            int moveTokenToX; // x coordinate for tile to move token to
+            int moveTokenToY; // y coordinate for tile to move token to
+            public void mouseClicked(MouseEvent me)
             {
-                cellWidth = mx / ppi; // use integer division to determine the x coordinate (in the array) of the cell clicked
-                cellHeight = my / ppi; // use integer division to determine the y coordinate (in the array) of the cell clicked
-                System.out.println("User clicked in cell: (" + cellWidth + ", " + cellHeight + ")"); // print coordinates (in the array) of mouse click to console
+                f.repaint();
+                int mx = me.getX(); // get the X coordinate of the mouse click
+                int my = me.getY(); // get the Y coordinate of the mouse click
+                int cellWidth; // variable for x coordinate of the cell if the user clicked on the grid
+                int cellHeight; // variable for y coordinate of the cell if the user clicked on the grid
 
-                if (!tokenSelected)
-                {
-                    tokenToMoveX = cellWidth; // set the x coordinate of the cell clicked
-                    tokenToMoveY = cellHeight; // set the y coordinate of the cell clicked
-                    tokenSelected = true; // set tokenSelected to true for next iteration
-                }
+                if (mx < marginSize && my < marginSize)
+                    System.exit(0); // quit the program if the user clicks the top left of the screen
+
+                if (mx < marginSize || my < marginSize || mx > (screenWidth - marginSize) || my > (screenHeight - marginSize))
+                    System.out.println("User clicked at position: (" + mx + ", " + my + ")"); // print coordinates (in pixels) of mouse click to console
                 else
                 {
-                    moveTokenToX = cellWidth; // set the x coordinate of the cell
-                    moveTokenToY = cellHeight; // set the y coordinate of the cell
-                    tokenSelected = false; // set tokenSelected back to false for next iteration
+                    cellWidth = mx / ppi; // use integer division to determine the x coordinate (in the array) of the cell clicked
+                    cellHeight = my / ppi; // use integer division to determine the y coordinate (in the array) of the cell clicked
+                    System.out.println("User clicked in cell: (" + cellWidth + ", " + cellHeight + ")"); // print coordinates (in the array) of mouse click to console
 
-                    if (mapArray[moveTokenToY][moveTokenToX].getToken() == null) // check that target cell is empty
+                    if (!tokenSelected)
                     {
-                        mapArray[moveTokenToY][moveTokenToX].setToken(mapArray[tokenToMoveY][tokenToMoveX].getToken()); // copy the selected token into its new position
-                        mapArray[tokenToMoveY][tokenToMoveX].setToken(null); // delete the selected token from its old position
+                        tokenToMoveX = cellWidth; // set the x coordinate of the cell clicked
+                        tokenToMoveY = cellHeight; // set the y coordinate of the cell clicked
+                        tokenSelected = true; // set tokenSelected to true for next iteration
                     }
                     else
-                        System.err.println("error: target cell is not empty!"); // if target cell is not empty, print an error message
+                    {
+                        moveTokenToX = cellWidth; // set the x coordinate of the cell
+                        moveTokenToY = cellHeight; // set the y coordinate of the cell
+                        tokenSelected = false; // set tokenSelected back to false for next iteration
+
+                        if (mapArray[moveTokenToY][moveTokenToX].getToken() == null) // check that target cell is empty
+                        {
+                            mapArray[moveTokenToY][moveTokenToX].setToken(mapArray[tokenToMoveY][tokenToMoveX].getToken()); // copy the selected token into its new position
+                            mapArray[tokenToMoveY][tokenToMoveX].setToken(null); // delete the selected token from its old position
+                        }
+                        else
+                            System.err.println("error: target cell is not empty!"); // if target cell is not empty, print an error message
+                    }
+                    f.repaint();
                 }
-                repaint();
             }
-        }
-        @Override public void mousePressed(MouseEvent e) { }
-        @Override public void mouseReleased(MouseEvent e) { }
-        @Override public void mouseEntered(MouseEvent e) { }
-        @Override public void mouseExited(MouseEvent e) { }
-    
-        // constructor for class - requires marginSize
-        MouseInput(int tempMarginSize, Tile[][] tempMapArray, int tempPPI, int tempHeight, int tempWidth)
-        {
-            this.marginSize = tempMarginSize;
-            this.mapArray = tempMapArray;
-            this.ppi = tempPPI;
-            this.screenHeight = tempHeight;
-            this.screenWidth = tempWidth;
-        }
+            @Override
+            public void mousePressed(MouseEvent e) { }
+            @Override
+            public void mouseReleased(MouseEvent e) { }
+            @Override
+            public void mouseEntered(MouseEvent e) { }
+            @Override
+            public void mouseExited(MouseEvent e) { }
+        }); // add mouse listener
     }
 
     public static void main(String[] args)
